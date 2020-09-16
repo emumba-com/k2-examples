7c0e3664ec72ca307a541a74fca1837bf665fd7b
Abdullah Razzaki <syedabdullahrazzaki@gmail.com>
2020-09-16 15:16:05 +0500

    Updates modal UI (#14)

--- CRA-Example/src/components/BubbleChartWithDrilldown/BubbleChart.style.ts
+++ CRA-Example/src/components/BubbleChartWithDrilldown/BubbleChart.style.ts
@@ -2,7 +2,41 @@ import styled from "styled-components";
 import { Modal } from "antd";
 
 export const ModalWrapper = styled(Modal)`
-  .ant-modal-body {
-    height: 400px;
+  .ant-modal-content,
+  .ant-modal-header {
+    background: ${({ theme }) => {
+      return theme.backgroundColors.card;
+    }};
+    border-bottom: none;
+    .ant-modal-title,
+    .ant-modal-close-x {
+      color: ${({ theme }) => {
+        return theme.textColors.heading;
+      }};
+    }
+  }
+  .tooltip.type-dark.show {
+    > div > div {
+      background: none !important;
+    }
+  }
+  .modal-chart-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 60vw;
+    width: 60vw;
+    min-width: 60vw;
+    > div {
+      width: 100%;
+    }
+  }
+`;
+
+export const ChartWrapper = styled.div`
+  .k2--d3-chart-inner-wrapper {
+    circle {
+      cursor: pointer;
+    }
   }
 `;
