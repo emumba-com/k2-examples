1b1a9cc512e6bc6505f8b9a6a03702fdc70608ff
Muhammad Usman <Muhammad-Usman17@users.noreply.github.com>
2023-01-05 18:28:19 +0500

    Update mock-server.js

--- Mock-Server/mock-server.js
+++ Mock-Server/mock-server.js
@@ -3,6 +3,6 @@
 const http = require('http')
 const mockserver = require('mockserver')
 
-http.createServer(mockserver('../Mock-Server/mock')).listen(7000)
+http.createServer(mockserver('../Mock-Server/mock')).listen(7000,"127.0.0.1")
 
 console.log('Mock server started on port 7000...')
